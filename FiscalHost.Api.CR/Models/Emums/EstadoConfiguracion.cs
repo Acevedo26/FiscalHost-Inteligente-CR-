86b8cbf75875e0e334db2e98be7323254b56d755
José Andrés <joseacevedofallas@gmail.com>
2026-06-19 15:42:34 -0600

    cleanup: eliminar migraciones antiguas de SqlServer y corregir carpeta typo Emums

--- FiscalHost.Api.CR/Models/Emums/EstadoConfiguracion.cs
+++
@@ -1,8 +0,0 @@
-namespace FiscalHost.Api.CR.Models.Emums;
-
-public enum EstadoConfiguracion
-{
-    Activa = 1,
-    Inactiva = 2,
-    PendienteValidacion = 3
-}
--- FiscalHost.Api.CR/Models/Emums/TipoIdentificacion.cs
+++
@@ -1,9 +0,0 @@
-namespace FiscalHost.Api.CR.Models.Emums;
-
-public enum TipoIdentificacion
-{
-    CedulaFisica,
-    CedulaJuridica,
-    DIMEX,
-    NITE
-}
